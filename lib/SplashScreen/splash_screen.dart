cfd781988e2c0761782d513c419ec12d2599a5c3
Subhajit-14th <subhajitdtt700@gmail.com>
2025-02-19 23:21:58 +0530

    app logo and app name was changed and add splash screen is added

---
+++ lib/SplashScreen/splash_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  double _opacity = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _startAnimation();
+  }
+
+  void _startAnimation() {
+    Future.delayed(Duration(milliseconds: 500), () {
+      setState(() {
+        _opacity = 1.0;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: AnimatedOpacity(
+          duration: Duration(seconds: 3),
+          opacity: _opacity,
+          child: Image.asset(
+            'assets/app_logo/app_logo.jpg',
+            width: double.infinity,
+            height: 180,
+          ),
+        ),
+      ),
+    );
+  }
+}
