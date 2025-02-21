80b62597a52d51c34b48b84eea79f0f48a998814
Subhajit-14th <subhajitdtt700@gmail.com>
2025-02-22 04:21:59 +0530

    DI file name correction and some changes inside DI file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
+import 'package:the_fin_news/services/depency_services.dart';
 import 'package:the_fin_news/utils/assets/app_colors.dart';
 import 'package:the_fin_news/view/SplashScreen/splash_screen.dart';
 import 'package:the_fin_news/viewModel/live_news_provider.dart';
@@ -14,6 +15,9 @@ void main() {
     statusBarColor: AppColor.primaryColor, // Set the desired color
     statusBarIconBrightness: Brightness.light, // Light or dark icons
   ));
+
+  /// set dependency injection
+  setupServiceLocator();
 
   runApp(
     MultiProvider(
